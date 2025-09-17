7f539effba561e5b56158a9a2e10c935bcd525e3
theLudovyc <theludovyc@gmail.com>
2025-09-17 22:06:56 +0200

    update map (#8)
    infinite positions

--- scenes/Game/map.gd
+++ scenes/Game/map.gd
@@ -5,16 +5,21 @@ var godot = preload("res://icon.svg")
 
 var tile_size = 128
 
-var available_pos:PackedVector2Array = [
-	Vector2i(1, 0),
-	Vector2i(1, 1),
-	Vector2i(0, 1),
-	Vector2i(-1, 1),
-	Vector2i(-1, 0),
-	Vector2i(-1, -1),
-	Vector2i(0, -1),
-	Vector2i(1, -1),
-]
+var available_pos:PackedVector2Array
+
+var available_pos_rect := Rect2i(-1, -1, 3, 3)
+
+func add_new_rectangle_pts():
+	for i in range(available_pos_rect.size.x):
+		for j in range(available_pos_rect.size.y):
+			if (i == 0 or i == available_pos_rect.size.x - 1) or\
+				(j == 0 or j == available_pos_rect.size.y - 1):
+				available_pos.append(
+					Vector2i(
+						available_pos_rect.position.x + i,
+						available_pos_rect.position.y + j
+				))
+	pass
 
 func instantiate_building(pos:Vector2i):
 	var sprite = Sprite2D.new()
@@ -26,6 +31,12 @@ func instantiate_building(pos:Vector2i):
 	add_child(sprite)
 	
 func get_random_pos() -> Vector2i:
+	if available_pos.is_empty():
+		available_pos_rect.position -= Vector2i.ONE
+		available_pos_rect.size += Vector2i(2, 2)
+		
+		add_new_rectangle_pts()
+	
 	var index = randi_range(0, available_pos.size() - 1)
 	
 	var pos = available_pos[index]
@@ -38,6 +49,8 @@ func add_new_building_randomly():
 	instantiate_building(get_random_pos())
 
 func _ready() -> void:
+	add_new_rectangle_pts()
+	
 	var sprite = Sprite2D.new()
 	
 	sprite.texture = godot
